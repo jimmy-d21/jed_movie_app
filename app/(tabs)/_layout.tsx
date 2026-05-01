ac3c7ff038e3e8304782520ff92c8937853fd249
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-05-01 19:49:49 +0800

    Provide all the tabs lists

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,14 @@
+import { Tabs } from "expo-router";
+
+const _TabLayout = () => {
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen name="search" options={{ title: "Search" }} />
+      <Tabs.Screen name="saved" options={{ title: "Saved" }} />
+      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+    </Tabs>
+  );
+};
+
+export default _TabLayout;
